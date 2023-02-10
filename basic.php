5dc80f67a3ecee5c872cd4c0c90af891fba6e8d5
Tony <61390876+PtCrust@users.noreply.github.com>
2023-02-10 16:04:28 +0200

    Update basic.php

--- basic.php
+++ basic.php
@@ -7,20 +7,26 @@ $dbname = "productsDB";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// $sql = "CREATE DATABASE productsDB";
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
+// I used PhpMyAdmin to create Database
+// but here is the SQL to create table
+
+// $sql = "CREATE DATABASE productsDB";
 
 $sql = "CREATE TABLE products (
     id INT() UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    SKU VARCHAR(120) NOT NULL,
-    name VARCHAR(120) NOT NULL,
-    price INT()
+    SKU VARCHAR(120) NULL INDEX KEY,
+    name VARCHAR(120) NULL,
+    price FLOAT() NULL,
+    size INT() NOT NULL,
+    height INT() NOT NULL,
+    width INT() NOT NULL,
+    length INT() NOT NULL,
+    weight FLOAT() NOT NULL,
     )";
     
     
